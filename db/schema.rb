40b0e7ead3406fca03c45c17e46dd228810e39e8
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-02-27 16:47:23 -0300

    Cria migração final de produto (BigDecimal).

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_152008) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_171546) do
+  create_table "products", force: :cascade do |t|
+    t.integer "store_id", null: false
+    t.string "title"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["store_id"], name: "index_products_on_store_id"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "stores"
 end
