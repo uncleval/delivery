7c4c092e45d34ca527f8d45077bb8dcb8f4f751c
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-03-19 15:55:01 -0300

    Adiciona user auth aos testes de request.

--- spec/requests/stores_spec.rb
+++ spec/requests/stores_spec.rb
@@ -13,16 +13,22 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/stores", type: :request do
-  
-  # This should return the minimal set of attributes required to create a valid
-  # Store. As you add validations to Store, be sure to
-  # adjust the attributes here as well.
+  let(:user) {
+    user = User.new(email: "user@example.com", password: "123456", password_confirmation: "123456")
+    user.save!
+    user
+  }
+
   let(:valid_attributes) {
-    {name: "Great Restaurant"}
+    {name: "Great Restaurant", user: user}
   }
 
   let(:invalid_attributes) {
     {name: nil}
+  }
+
+  before {
+    sign_in(user)
   }
 
   describe "GET /index" do
