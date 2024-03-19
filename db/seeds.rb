f78d64235f11db7c409a1d66ea4268a786ca0cfd
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-03-19 12:22:33 -0300

    Usa seeds para garantir existência de lojas.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.find_by(email: "store@example.com")
+if !user
+  user = User.new(
+    email: "store@example.com",
+    password: "123456",
+    password_confirmation: "123456"
+  )
+  user.save!
+end
+
+["Orange Curry", "Belly King"].each do |store|
+  Store.find_or_create_by!(name: store, user: user)
+end
+
+[
+  "Massaman Curry",
+  "Risotto with Seafood",
+  "Tuna Sashimi",
+  "Fish and Chips",
+  "Pasta Carbonara"
+].each do |dish|
+  store = Store.find_by(name: "Orange Curry")
+  Product.find_or_create_by!(title: dish, store: store)
+end
+
+[
+  "Mushroom Risotto",
+  "Caesar Salad",
+  "Mushroom Risotto",
+  "Tuna Sashimi",
+  "Chicken Milanese"
+].each do |dish|
+  store = Store.find_by(name: "Belly King")
+  Product.find_or_create_by!(title: dish, store: store)
+end
