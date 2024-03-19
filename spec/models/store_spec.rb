f3deb7269397bd72a729fab274918e74a088aa6a
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-03-19 15:58:58 -0300

    Garante relacionamento :store belongs_to :user

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Store, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_length_of(:name).is_at_least(3) }
+    it { should belong_to(:user) }
   end
 end
