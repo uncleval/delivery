b7d77dc9fb9db4500e03ee237471749f100446d8
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-03-19 12:16:32 -0300

    Garante que rota de entrada está definida.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :stores
   get "listing" => "products#listing"
+  root to: "welcome#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
