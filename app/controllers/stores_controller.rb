4dadae3d8f9a4d003d8b36d3c0834a873a9a6869
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-04-30 10:47:57 -0300

    Permite que `admin` crie e liste lojas.

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,7 +4,11 @@ class StoresController < ApplicationController
 
   # GET /stores or /stores.json
   def index
-    @stores = Store.all
+    if current_user.admin?
+      @stores = Store.all
+    else
+      @stores = Store.where(user: current_user)
+    end
   end
 
   # GET /stores/1 or /stores/1.json
@@ -23,7 +27,10 @@ class StoresController < ApplicationController
   # POST /stores or /stores.json
   def create
     @store = Store.new(store_params)
-    @store.user = current_user
+    if !current_user.admin?
+      @store.user = current_user
+    end
+    # @store.user = current_user
 
     respond_to do |format|
       if @store.save
@@ -67,6 +74,14 @@ class StoresController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def store_params
-      params.require(:store).permit(:name)
+      # params.require(:store).permit(:name)
+
+      required = params.require(:store)
+
+      if current_user.admin?
+        required.permit(:name, :user_id)
+      else
+        required.permit(:name)
+      end
     end
 end
