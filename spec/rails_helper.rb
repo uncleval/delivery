9e02b9176c1155737a12db021caa52b02c844e90
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-04-30 00:17:46 -0300

    Protege API de stores com Token Auth.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,8 +30,29 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
+module APIRequestHelpers
+  def api_sign_in(user, credential)
+    post(
+      "/sign_in",
+      headers: {
+        "Accept" => "application/json",
+        "X-API-KEY" => credential.key
+      },
+      params: {
+        login: {
+          email: user.email,
+          password: user.password
+        }
+      }
+    )
+
+    JSON.parse(response.body)
+  end
+end
+
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include APIRequestHelpers, type: :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
