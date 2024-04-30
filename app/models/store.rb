7c8602037b6bbc8d5d7e84aa8b7c2a7e88e0fa60
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-04-30 00:00:56 -0300

    Garante que loja pertence sempre a um seller.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,11 @@
 class Store < ApplicationRecord
   belongs_to :user
+  before_validation :ensure_seller
   validates(:name, {presence: true, length: {minimum: 3}})
+
+  private
+
+  def ensure_seller
+    self.user = nil if self.user && !self.user.seller?
+  end
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Store, type: :model do
     it { should validate_length_of(:name).is_at_least(3) }
     it { should belong_to(:user) }
   end
+
+  describe "belongs_to" do
+    let(:admin) {
+      User.create!(
+        email: "admin@example.com",
+        password: "123456",
+        password_confirmation: "123456",
+        role: :admin
+      )
+    }
+
+    it "should not belong to admin users" do
+      store = Store.create(name: "store", user: admin)
+
+      expect(store.errors.full_messages).to eq ["User must exist"]
+    end
+  end
 end
